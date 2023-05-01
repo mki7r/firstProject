290e8203d4f9494863a9fab2db228e2373d9d408
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 05:49:08 +0300

    Create Date.java

---
+++ project oop/Date.java
@@ -0,0 +1,46 @@
+public class Date {
+
+    private int day;
+    private int month;
+    private int year;
+  
+  
+    public Date(int day, int month, int year) {
+    
+    
+    
+    
+    }
+      public Date() {
+    }
+
+    public int getDay() {
+      
+    }
+
+    public void setDay(int day) {
+        
+    }
+  
+    public int getMonth() {
+        
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        ;
+    }
+
+    public void setYear(int year) {
+      
+    }
+
+    @Override
+    public String toString() {
+        
+    }
+  
+}
