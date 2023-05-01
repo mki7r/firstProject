e57c4523a5fa5fc2f9e50b999c55b13bdd8306bc
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 06:03:45 +0300

    Create Seat.java

---
+++ project oop/Seat.java
@@ -0,0 +1,44 @@
+public abstract class Seat {
+    protected int row;
+    protected int col;
+    protected String color;
+
+    public Seat() {
+    }
+
+    public Seat() {
+    
+    }
+
+    public int getRow() {
+      
+    }
+
+    public void setRow(int row) {
+     
+    }
+
+    public int getCol() {
+     
+    }
+
+    public void setCol(int col) {
+      
+    }
+
+    public String getColor() {
+      
+    }
+
+    public void setColor(String color) {
+      
+    }
+public abstract void displayClass();
+
+
+    @Override
+    public String toString() {
+      
+    }
+    
+}
