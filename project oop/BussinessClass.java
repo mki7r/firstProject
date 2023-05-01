38613383af7cfa25ca4cfc2791a64634ac054a16
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 05:28:54 +0300

    Create BussinessClass.java

---
+++ project oop/BussinessClass.java
@@ -0,0 +1,20 @@
+public class BussinessClass extends Seat {
+
+
+}
+ public BussinessClass(String  company, int row, int col, String color){
+ 
+ 
+ 
+ 
+ }
+ 
+ 
+ public BussinessClass(int row, int col, String color) {
+        super(row, col, color);
+    }
+    
+        @Override
+    public void displayClass() {
+    
+    }
