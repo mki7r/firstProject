82c800f547b499e529c61f3baaec7f016ec4fcc4
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 05:41:03 +0300

    Create City.java

---
+++ project oop/City.java
@@ -0,0 +1,43 @@
+public class City {
+  private String Name;
+     private String  IATACode ;
+ private String Guid ;
+
+    public City() {
+    }
+    
+    public City() {
+    
+    
+    }
+    
+    public void setName(String Name) {
+    
+    
+    }
+    
+    public String getIATACode() {
+    
+    }
+    
+     public void setIATACode(String IATACode) {
+     
+     
+     }
+     
+      public String getGuid() {
+      
+      
+      }
+      
+       public void setGuid(String Guid) {
+       
+       
+       }
+       
+       
+        @Override
+    public String toString() {
+    
+    
+    }
