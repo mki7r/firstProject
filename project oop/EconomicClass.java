168b387a9d5bd778fe872b29810f5abddc0d35c3
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 05:50:28 +0300

    Create EconomicClass.java

---
+++ project oop/EconomicClass.java
@@ -0,0 +1,27 @@
+public class EconomicClass extends Seat {
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount() {
+        
+    }
+
+    double discount;
+    public EconomicClass() {
+        
+    }
+
+    public EconomicClass(int row, int col, String color) {
+       
+    }
+
+    
+
+    @Override
+    public void displayClass() {
+       
+    }
+
+    }
