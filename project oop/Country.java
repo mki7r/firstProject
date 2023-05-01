943a785367b94ed6afbb492bd3f18b7cffa6c876
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 05:43:45 +0300

    Create Country.java

---
+++ project oop/Country.java
@@ -0,0 +1,52 @@
+public class Country {
+    private String Name;
+     private String  IATACode ;
+ private String Guid ;
+
+    public Country() {
+    }
+  
+  public Country(String Name, String IATACode, String Guid) {
+  
+  
+  }
+  
+   public String getName() {
+   
+   
+   }
+  
+     public void setName(String Name) {
+     
+     
+     
+     }
+  
+  
+     public String getIATACode() {
+     
+     
+     
+     }
+  
+      public void setIATACode(String IATACode) {
+      
+      
+      }
+    
+    public String getGuid() {
+    
+    
+    }
+  
+  
+     public void setGuid(String Guid) {
+     
+     
+     }
+  
+  
+     public String toString() {
+     
+     
+     }
