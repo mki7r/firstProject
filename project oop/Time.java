e10817d728153fb0498740fbb52486b6e385b95a
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 06:07:24 +0300

    Create Time.java

---
+++ project oop/Time.java
@@ -0,0 +1,33 @@
+public class Time {
+        
+  
+
+    public Time() {
+
+    }
+
+    public Time() {
+    }
+
+    public int gethour() {
+        
+    }
+
+    public void sethour() {
+       
+    }
+
+    public int getmin() {
+     
+    }
+
+    public void setmin() {
+      
+    }
+
+    
+    @Override
+    public String toString() {
+       
+    }
+}
