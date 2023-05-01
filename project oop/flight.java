540563649dfd413f66e1ab3fbbab4654dfdbdf4a
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 05:54:34 +0300

    Create flight.java

---
+++ project oop/flight.java
@@ -0,0 +1,107 @@
+public class Flight {
+
+    private String Flightnumber;
+    private Country Departurecountry;
+    private City Departurecity;
+    private Country Arrivalcountry;
+    private City ArrivalCity;
+    private String Duration;
+    private Date flightDate;
+    private String Airline;
+    private Time Departuretime;
+    private Time Arrivaltime;
+
+    public Flight() {
+    }
+
+    public Flight() {
+  
+    }
+
+    public String getFlightnumber() {
+        
+    }
+
+    public void setFlightnumber(String Flightnumber) {
+       
+    }
+
+    public Country getDeparturecountry() {
+        
+    }
+
+    public void setDeparturecountry(Country Departurecountry) {
+       
+    }
+
+    public City getDeparturecity() {
+       
+    }
+
+    public void setDeparturecity(City Departurecity) {
+        
+    }
+
+    public Country getArrivalcountry() {
+       
+    }
+
+    public void setArrivalcountry(Country Arrivalcountry) {
+        
+    }
+
+    public City getArrivalCity() {
+        
+    }
+
+    public void setArrivalCity(City ArrivalCity) {
+     
+    }
+
+    public String getDuration() {
+       
+    }
+
+    public void setDuration(String Duration) {
+        
+    }
+
+    public Date getFlightDate() {
+       
+    }
+
+    public void setFlightDate(Date flightDate) {
+        
+    }
+
+    public String getAirline() {
+        
+    }
+
+    public void setAirline(String Airline) {
+        
+    }
+
+    public Time getDeparturetime() {
+     
+    }
+
+    public void setDeparturetime(Time Departuretime) {
+     
+    }
+
+    public Time getArrivaltime() {
+       
+    }
+
+    public void setArrivaltime(Time Arrivaltime) {
+        
+    }
+
+    @Override
+    public String toString() {
+       
+    }
+    
+
+}
