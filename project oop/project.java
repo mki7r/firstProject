9c1e68e3b31ba962d14a8b5eecd87ee81e918ea9
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 05:55:51 +0300

    Create project.java

---
+++ project oop/project.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Project {
+     static ArrayList<Flight> allflights;
+    static Scanner scan = new Scanner(System.in);
+    static Ticket t = null;
+
+    public static void main(String[] args) {
+    
+    
+    }
+  
+}
