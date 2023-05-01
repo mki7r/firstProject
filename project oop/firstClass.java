35b07522f41ec63e8b15f4c3e2cc7ea051a014ce
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 05:52:19 +0300

    Create firstClass.java

---
+++ project oop/firstClass.java
@@ -0,0 +1,35 @@
+public class FirstClass extends Seat {
+    String fclass;
+    String privalage;
+
+    public FirstClass() {
+        
+    }
+
+    public FirstClass() {
+        super();
+    }
+
+    public String getFclass() {
+        
+    }
+
+    public void setFclass() {
+        
+    }
+
+    public String getPrivalage() {
+       
+    }
+
+    public void setPrivalage(String privalage) {
+       
+    }
+
+    @Override
+    public void displayClass() {
+       
+
+    }
+    
+}
