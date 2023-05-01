45319bba494e11cd2e9e8f53b41d4444e8d1ac07
Husain ALsaigh <119853307+HUsainn3@users.noreply.github.com>
2023-05-01 06:06:08 +0300

    Create Ticket.java

---
+++ project oop/Ticket.java
@@ -0,0 +1,96 @@
+public class Ticket implements SeatPriceCalcutator {
+    private Flight flight;
+private Seat seat;
+private String Name;
+private Long Phone;
+private String Passport;
+private String Email;
+private double Price;
+public static int Ticketnum=0;
+
+    public Ticket() {
+      
+    }
+
+    public Flight getFlight() {
+        
+    }
+
+    public void setFlight(Flight flight) {
+        
+    }
+
+    public Seat getSeat() {
+       
+    }
+
+    public void setSeat(Seat seat) {
+        
+    }
+
+    public String getName() {
+       
+    }
+
+    public void setName(String Name) {
+       
+    }
+
+    public Long getPhone() {
+      
+    }
+
+    public void setPhone(Long Phone) {
+        
+    }
+
+    public String getPassport() {
+        
+    }
+
+    public void setPassport(String Passport) {
+        
+    }
+
+    public String getEmail() {
+      
+    }
+
+    public void setEmail(String Email) {
+        
+    }
+
+    public double getPrice() {
+      
+    }
+
+    public void setPrice(double Price) {
+        
+    }
+
+    public Ticket() {
+      
+    }
+
+    @Override
+    public String toString() { 
+        
+    }
+
+    @Override
+    public double SeatPrice() {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
